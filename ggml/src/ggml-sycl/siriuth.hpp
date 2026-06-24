98f1914d3355b5369700b71524d515df2ebe0eda
Siriuth <104502768+siriuth@users.noreply.github.com>
2026-06-24 17:21:49 +0900

    Add files via upload

--- ggml/src/ggml-sycl/siriuth.hpp
+++ ggml/src/ggml-sycl/siriuth.hpp
@@ -1,6 +1,8 @@
 #ifndef SIRIUTH_WRAPPER_HPP
 #define SIRIUTH_WRAPPER_HPP
 
+// 2026/06/23 wait()表示をコメントアウト
+
 //#include "siriuth.hpp"
 #include "common.hpp"
 #include <functional>
@@ -47,7 +49,7 @@ void ggml_sycl_looper(
                 const int global2 = MAX(MIN(group_cnt2-i2, MIN(group_size, max_work_group_size)), 1);
 
                 if(wait_cnt++%SYCL_QUEUE_WAIT_TIMMING==0){
-                    GGML_SYCL_DEBUG("[SYCL] %s wait()\n", __func__);
+                    //GGML_SYCL_DEBUG("[SYCL] %s wait()\n", __func__);
                     SYCL_CHECK(CHECK_TRY_ERROR((stream)->wait()));
                 }
 
@@ -63,6 +65,116 @@ void ggml_sycl_looper(
             i1+=global1;
         }
         i0+=global0;
+    }
+}
+
+template <typename SubmitFunc>
+void ggml_sycl_looper(
+        const int size_range,
+        const int local,
+        const int group_size,
+        queue_ptr stream,
+        SubmitFunc submit_func)
+{
+    sycl::device dev = stream->get_device();
+    const int max_work_group_size = dev.get_info<sycl::info::device::max_work_group_size>();
+
+    int group_cnt = (size_range + local -1)/local;
+
+    int wait_cnt = 0;
+    int i = 0;
+    while(i < group_cnt){
+        const int global = MAX(MIN(group_cnt-i, MIN(group_size, max_work_group_size)), 1);
+
+        if(wait_cnt++%SYCL_QUEUE_WAIT_TIMMING==0){
+            //GGML_SYCL_DEBUG("[SYCL] %s wait()\n", __func__);
+            SYCL_CHECK(CHECK_TRY_ERROR((stream)->wait()));
+        }
+
+        submit_func(
+            (global * local),
+            (i * local)
+        );
+
+        i+=global;
+    }
+}
+
+// 範囲外に出さない処理
+template <typename SubmitFunc>
+void ggml_sycl_adjusted_looper(
+        const int size_range,
+        const int local,
+        const int group_size,
+        queue_ptr stream,
+        SubmitFunc submit_func)
+{
+    sycl::device dev = stream->get_device();
+    const int max_work_group_size = dev.get_info<sycl::info::device::max_work_group_size>();
+
+    int group_cnt = (size_range + local -1)/local;
+
+    int adjusted_group_quantity = 0;
+    int wait_cnt = 0;
+    int i = 0;
+    //GGML_SYCL_DEBUG("[SYCL] %s size_range:%d local:%d group_size:%d group_cnt:%d\n", __func__, size_range, local, group_size, group_cnt);
+    while(i < group_cnt){
+        adjusted_group_quantity = MAX(MIN(group_cnt-i, MIN(group_size, max_work_group_size)), 1);
+        //GGML_SYCL_DEBUG("[SYCL] %s i:%d adjusted_group_quantity:%d\n", __func__, i, adjusted_group_quantity);
+
+        // 残りの数 ＞＝ 今回の数 普通の処理
+        // 残りの数 ＜ 今回の数 端数処理へ 端数処理ではlocalのブロックを一括で出力して最後の一つをグローバルとローカルを揃えて出力
+        //if(size_range-(group_cnt-i)*local < adjusted_group_quantity * local){
+        if(size_range-i*local < adjusted_group_quantity * local){
+            break;
+        }
+
+        if(wait_cnt++%SYCL_QUEUE_WAIT_TIMMING==0){
+            //GGML_SYCL_DEBUG("[SYCL] %s wait()\n", __func__);
+            SYCL_CHECK(CHECK_TRY_ERROR((stream)->wait()));
+        }
+
+        submit_func(
+            (adjusted_group_quantity * local),
+            local,
+            (i * local)
+        );
+
+        i += adjusted_group_quantity;
+    }
+    // 端数処理
+    if(i < group_cnt){
+        // 端数処理
+        if(size_range - i * local - adjusted_group_quantity * local == 0){
+            GGML_SYCL_DEBUG("[SYCL] %s i:%d adjusted_group_quantity:%d local:%d\n", __func__, i, adjusted_group_quantity, local);
+            submit_func(
+                (adjusted_group_quantity * local),
+                local,
+                (i * local)
+            );
+            i += adjusted_group_quantity;
+        } else {
+           // local単位で処理を行う。
+           if(adjusted_group_quantity > 1){
+                const int re_adjusted_group_quantity = adjusted_group_quantity - 1;
+                GGML_SYCL_DEBUG("[SYCL] %s i:%d re_adjusted_group_quantity:%d local:%d\n", __func__, i, re_adjusted_group_quantity, local);
+                submit_func(
+                    (re_adjusted_group_quantity * local),
+                    local,
+                    (i * local)
+                );
+                i += re_adjusted_group_quantity;
+            }
+            // localで割り切れなかった余りの処理
+            const int adjusted_local = MAX(MIN(size_range - (group_cnt-i)*local, local), 1);
+            GGML_SYCL_DEBUG("[SYCL] %s i:%d adjusted_local:%d local:%d\n", __func__, i, adjusted_local, local);
+            submit_func(
+                    adjusted_local,
+                    adjusted_local,
+                    (i * local)
+            );
+            i += 1;
+        }
     }
 }
 
